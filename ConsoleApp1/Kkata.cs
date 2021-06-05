bfcc700d8d6aa48a409091ee043ce833dbcf4d15
Adam <adam.gebicki01@gmail.com>
2021-06-05 15:51:47 +0200

    First commit & first solution :)
    Signed-off-by: Adam <adam.gebicki01@gmail.com>

---
+++ ConsoleApp1/Kkata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Kata
+    {
+        public static int Solution(int value)
+        {
+            int res = 0;
+            for (int index = 0; index < value; index++)
+            {
+                if (index % 3 == 0 || index % 5 == 0)
+                {
+                    res += index;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ ConsoleApp1/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, Kata.Solution(10));
+        }
+    }
+}
